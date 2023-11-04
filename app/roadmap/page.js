afc9f362c5da2eae56126b328c5d38735d665cae
ade <harounadelani@gmail.com>
2023-11-04 09:31:35 +0100

    roadmap page

---
+++ app/roadmap/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+export default function Roadmap() {
+    return (
+        <div>
+            <div className="flex flex-col">
+                <Image src={"/duck-17.svg"} height={100} width={100}/>
+
+            </div>
+            
+        </div>
+    )
+}
